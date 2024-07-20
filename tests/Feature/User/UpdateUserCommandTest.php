5b57a6af61be0a039a52107ac74b785d0d843c66
Lucas Rigon <lucas@bussolasocial.com.br>
2024-07-19 22:15:47 -0300

    update user testes

--- tests/Feature/User/UpdateUserCommandTest.php
+++ tests/Feature/User/UpdateUserCommandTest.php
@@ -2,32 +2,41 @@
 
 namespace Tests\Feature\User;
 
+use App\Models\User;
 use Core\Modules\User\Commons\Entities\UserEntity;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Infra\Persistence\User\Command\UserCommand;
 use Tests\TestCase;
 
-class CreateUserCommandTest extends TestCase
+class UpdateUserCommandTest extends TestCase
 {
     use DatabaseMigrations;
 
-    public function test_deve_testar_create_de_um_usuario(): void
+    public function test_deve_testar_atualizar_um_usuario(): void
     {
         // Arrange
+        $userModel = User::factory()->create();
         $userCommand = new UserCommand();
+
         $userEntity = new UserEntity(
             name: 'name 2',
-            email: 'email 3',
+            email: 'email@3',
             password: 'password',
             age: 18
         );
+        $userEntity->setId($userModel->id);
+
         // Act
-        $userCommand->create($userEntity);
+        $userCommand->update($userEntity);
 
         // Assert
         $this->assertDatabaseHas('users', [
+            'id' => $userModel->id,
             'name' => $userEntity->getName(),
             'email' => $userEntity->getEmail()
         ]);
+
+        $this->assertNotEquals($userModel->name, $userEntity->getName());
+        $this->assertNotEquals($userModel->email, $userEntity->getEmail());
     }
 }
