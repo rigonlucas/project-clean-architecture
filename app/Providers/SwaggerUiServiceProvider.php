a542b079f024da4f4088f8354f7ac25fefacc5fe
rigonlucas <lucas.gelhen.rigon@gmail.com>
2024-10-15 13:16:15 -0300

    ajuste

--- app/Providers/SwaggerUiServiceProvider.php
+++ app/Providers/SwaggerUiServiceProvider.php
@@ -7,12 +7,13 @@ use Illuminate\Support\ServiceProvider;
 
 class SwaggerUiServiceProvider extends ServiceProvider
 {
-    public function boot() : void
+    public function boot(): void
     {
         Gate::define('viewSwaggerUI', function ($user = null) {
-            return in_array(optional($user)->email, [
+            /*return in_array(optional($user)->email, [
                 //
-            ]);
+            ])*/
+            return true;
         });
     }
 }
